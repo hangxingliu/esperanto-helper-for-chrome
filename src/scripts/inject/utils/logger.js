183a4ac946a3d32baf821f65a8ec3771063480e9
LiuYue <hangxingliu@gmail.com>
2017-08-17 22:07:15 +0800

    init commit

---
+++ src/scripts/inject/utils/logger.js
@@ -0,0 +1,29 @@
+//@ts-check
+
+const PREFIX = 'Esperanto Helper:';
+
+let debugInfos = [];
+
+let exportObject = { init, log, debugInfo, error };
+module.exports = exportObject;
+
+function log(...p) {
+	//eslint-disable-next-line no-console
+	console.log(PREFIX, ...p);
+}
+
+function debugInfo(...p) {
+	let info = p.join(' ');
+	debugInfos.push(info);
+}
+
+function error(description = '', ex = null) {
+	// TODO 通知用户发生错误了, 是否需要到 Github 去反馈
+	console.error(description, ex ? (ex.stack || ex) : '');
+}
+
+function init(injectScriptName) {
+	log(`${injectScriptName} has been injected!`);
+	return exportObject;
+}
+
